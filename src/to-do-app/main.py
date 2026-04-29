660bc9cd4d3bf01a3e97a8e92ffaef13b49d00f4
cjkarinen20 <cjkarinen20@gmail.com>
2026-04-29 14:24:42 -0400

    Re-organized Project and Added ToDoItem Class
    -Removed old commented-out code.
    -Added new comments for readibility.
    -Created the ToDoItem class.

--- src/to-do-app/main.py
+++ src/to-do-app/main.py
@@ -1,59 +1,67 @@
 from tkinter import * 
 import tkinter as tk
 
+# To-Do List Item Object Class
+class ToDoItem:
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+# Main Application Class
 class ToDoListApp:
     
     def __init__(self, root):
         
+        # -Application Window Title-
         root.title("To-Do List")
 
-
+        # -Application Window Frame-
         frame = Frame(root, borderwidth = 2, relief = "sunken")
         frame.grid(column = 1, row = 1, sticky = (N, E, S, W))
         root.columnconfigure(1, weight = 1)
         root.rowconfigure(1, weight = 1)
         
+        self.to_do_items = [
+            ToDoItem("Workout", "Push-Ups, Pull-Ups, Squats"),
+            ToDoItem("House Work", "Clean kitchen, Sweep floors, Do laundry"),
+            ToDoItem("Groceries", "Buy bread, milk, eggs"),
+        ]
+        
+        # -Label Text Widget-
         self.label_text = StringVar()
         label = Label(frame, text = "Some label text", textvariable = self.label_text)
-        #label.grid(column = 1, row = 1)
-        #label.pack(side = tk.LEFT)
-        
-        #label["text"] = "New label text"
-        #label["font"] = ("Courier", 40)
-        
+        # Configure the styling and sizing of the label text.
         label.configure(text = "New label text", font = ("Courier", 40))
         
+        # -Entry Text Display-
         self.entry_text = StringVar()
         entry = Entry(frame, textvariable = self.entry_text)
-        #entry.pack(side = tk.LEFT)
-        #entry.place(x = 100, y = 50)
         entry.grid(column = 2, row = 1)
-        
-        #label["textvariable"] = entry_text
-
+    
+        # -Input Box Button-
         button = Button(frame, text = "Button text", command = self.press_button)
-
         button.grid(column = 1, row = 2, sticky = (S, E, W))
         button.configure(width = 10, height = 2, font = ("Courier", 12))
-        #button.pack(side = tk.LEFT)
-        #button.place(x = 0, y = 0)
 
+        # -Dropdown List Widget-
         list_item_strings = ["Hey", "Hi", "Hello", "Howdy", "Greetings"]
         list_items = StringVar(value = list_item_strings)
         listbox = Listbox(frame, listvariable = list_items)
-        #listbox.pack(side = tk.LEFT, padx = 40, pady = 20)
         listbox["height"] = 3
         listbox.bind("<<ListboxSelect>>", lambda s: self.select_item(listbox.curselection()))
         listbox.grid(column = 2, row = 2)
         
+    # Button Press Logic Method
     def press_button(self):
         text = self.entry_text.get()
         self.label_text.set(text)
-    
+        
+    # Dropdown Select Item Logic Method
     def select_item(self, index):
         selected_item = self.list_item_strings[index[0]]
         print(selected_item)
         
 root = Tk()
-MyApp(root)
+ToDoListApp(root)
 root.mainloop()
